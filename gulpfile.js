8b4832fe89c3c25cee7e393971c7a11470104bf7
Vidal Rodrigo <vidalrodrigoti@gmail.com>
2020-10-28 20:44:33 -0300

    Primeira Versao

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+function css(){
+    return gulp 
+    .src('./scss/*scss')
+    .pipe(sass({
+        outputStyle:'expanded'
+    }))
+    .pipe(gulp.dest('css'));
+}
+
+function watchFiles(){
+    gulp.watch('scss/*.scss', css);
+    gulp.watch('index.html');
+}
+
+gulp.task('css',css);
+gulp.task('watch',gulp.parallel(watchFiles));
